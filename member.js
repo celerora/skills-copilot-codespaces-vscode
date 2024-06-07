a75b11fd919cf3812eaa22743691581aee9410c3
Miguel Alejandro Ortiz Perez M <miguel-ortiz-perez@hotmail.com>
2024-06-07 10:39:42 -0600

    Copilot second commit

--- member.js
+++ member.js
@@ -1,9 +1,12 @@
 function skillsMember() {
-  return {
-    restrict: 'E',
-    scope: {
-      member: '='
-    },
-    templateUrl: 'member.html'
-  };
+  return new Promise((resolve, reject) => {
+    const member = new Member();
+    member.getSkills()
+      .then(skills => {
+        resolve(skills);
+      })
+      .catch(err => {
+        reject(err);
+      });
+  });
 }
